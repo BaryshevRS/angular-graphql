c0a1f7ed310488dda0b97841d865589ed1ae58ff
BaryshevRS <baryshevrs@yandex.ru>
2021-12-28 04:04:45 +0300

    Added config for mongoDb

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,14 +4,22 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from "path";
 import { TodoModule } from './todo/todo.module';
+import { MongooseModule } from "@nestjs/mongoose";
+import 'dotenv/config.js';
 
 @Module({
-  imports: [GraphQLModule.forRoot({
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@mongocloud.4sqqe.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+    ),
+    GraphQLModule.forRoot({
     typePaths: ['**/*.graphql'],
     definitions: {
       path: join(process.cwd(), 'src/graphql.ts')
     }
-  }), TodoModule],
+  }),
+    TodoModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
